cd1b3dcd92ef066cbb203ffa702a4e6c8967538b
Chris Gilliam <Chris@comcorp.co.za>
2026-06-07 18:57:47 +0200

    Added LIdarService to console app

--- LidarSweeper/Program.cs
+++ LidarSweeper/Program.cs
@@ -25,14 +25,14 @@ using ILoggerFactory loggerFactory = LoggerFactory.Create(builder =>
 
 ILogger logger = loggerFactory.CreateLogger<Program>();
 
-var ultraborgAPI= host.Services.GetService<IUltraborgAPI>();
-if (ultraborgAPI != null)
-{
-     ultraborgAPI.Setup();
-     var ultraborg = ultraborgAPI?.Ultraborg;
-     var ultraborgServo = new UltraborgServo(4, 0,loggerFactory);
-     ultraborgServo.Init(ultraborg!);
-     int currentPosition = ultraborgServo.GetCurrentPosition();
+var ultraborgAPI= host.Services.GetRequiredService<IUltraborgAPI>();
+var lidar = host.Services.GetRequiredService<ILidarService>();
+
+    ultraborgAPI.Setup();
+    var ultraborg = ultraborgAPI?.Ultraborg;
+    var ultraborgServo = new UltraborgServo(4, 0,loggerFactory);
+    ultraborgServo.Init(ultraborg!);
+    int currentPosition = ultraborgServo.GetCurrentPosition();
     logger.LogInformation($"Current Position: {currentPosition}");
     int servoMax= ultraborgServo.ServoMax;
     int servoMin= ultraborgServo.ServoMin;
@@ -47,46 +47,14 @@ if (ultraborgAPI != null)
         Thread.Sleep(100);
         currentPosition = ultraborgServo.GetCurrentPosition();
         logger.LogInformation($"Current Position: {currentPosition}");
-    }
+        logger.LogInformation(
+        "Distance={Distance} cm  Strength={Strength}  Temp={Temperature:F1}°C",
+        lidar.Distance,
+        lidar.Strength,
+        lidar.Temperature);
 }
+
    
 
-
-//var raspberryPibrd = new RaspberryPiBoard();
-//var controller = raspberryPibrd.CreateGpioController();
-//int busNo = raspberryPibrd.GetDefaultI2cBusNumber();
-
-//using ILoggerFactory loggerFactory = LoggerFactory.Create(builder =>
-//{
-//    builder
-//        .SetMinimumLevel(LogLevel.Information)
-//        .AddConsole();
-//});
-
-//ILogger logger = loggerFactory.CreateLogger<Program>();
-
-//logger.LogInformation("Bus No: {BusNo}", busNo);
-
-//var ultraborg = new Ultraborg.Library.Ultraborg(logger);
-//Console.WriteLine("Scanning for Ultraborg address...");
-//int address = ultraborg.GetUltraBorgAdress();
-//if (address != -1)
-//{
-//    Console.WriteLine($"UltraBorg found on address {address}");
-//    ultraborg.Init(busNo, address);
-
-//    while (true)
-//    {
-//        var keyInfo = Console.ReadKey();
-//        switch (keyInfo.KeyChar)
-//        {
-
-//            case 'd': var distance = ultraborg.GetFilteredDistance(1); Console.WriteLine($"Filtered Distance: {distance}"); break;
-//            case 'u': var unfiltDistance = ultraborg.GetDistance(1); ; Console.WriteLine($"Unfiltered Distance: {unfiltDistance}"); break;
-//            case 'x': Environment.Exit(0); break;
-
-//        }
-//    }
-//}
 
 Console.ReadLine();
